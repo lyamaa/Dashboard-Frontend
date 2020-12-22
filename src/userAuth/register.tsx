92a495dac7a105d9d15f3617233ec7cf32054f1e
surajmt8848 <suraj8848mt@gmail.com>
2020-12-22 14:59:40 +0545

    create redirect

--- src/userAuth/register.tsx
+++ src/userAuth/register.tsx
@@ -3,38 +3,36 @@ import "./public.css";
 import Profile from "./img/profile.svg";
 import Access from "./img/register.svg";
 import Wave from "./img/wave.png";
-import axios from 'axios'
-import {Redirect} from 'react-router-dom'
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 export default class register extends Component {
-    username = ''
-    email = ''
-    password = ''
-    password_confirm = ''
+  username = "";
+  email = "";
+  password = "";
+  password_confirm = "";
 
-    state = {
-      redirect: false
-    }
+  state = {
+    redirect: false,
+  };
 
-    submit = async (e: SyntheticEvent) => {
-        e.preventDefault();
-        await axios.post('http://127.0.0.1:8000/api/register', {
-            username: this.username,
-            email: this.email,
-            password: this.password,
-            password_confirm: this.password_confirm
-        })
-        
-        this.setState({
-          redirect: true
-        })
-    }
+  submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    await axios.post("http://127.0.0.1:8000/api/register", {
+      username: this.username,
+      email: this.email,
+      password: this.password,
+      password_confirm: this.password_confirm,
+    });
 
+    this.setState({
+      redirect: true,
+    });
+  };
 
   render() {
-    
-   if(this.state.redirect){
-     return <Redirect to={'/login'}/>
-   }
+    if (this.state.redirect) {
+      return <Redirect to={"/login"} />;
+    }
     return (
       <div className="container">
         <div className="container">
@@ -58,7 +56,8 @@ export default class register extends Component {
                         className="input is-rounded"
                         type="text"
                         placeholder="Username"
-                        required onChange={e => this.username = e.target.value}
+                        required
+                        onChange={(e) => (this.username = e.target.value)}
                       />
                       <span className="icon is-small is-left">
                         <i className="fas fa-user"></i>
@@ -75,7 +74,7 @@ export default class register extends Component {
                         type="email"
                         placeholder="me@me.com"
                         required
-                        onChange={e => this.email = e.target.value}
+                        onChange={(e) => (this.email = e.target.value)}
                       />
                       <span className="icon is-small is-left">
                         <i className="fas fa-envelope"></i>
@@ -93,7 +92,7 @@ export default class register extends Component {
                         type="password"
                         placeholder="Password"
                         required
-                        onChange={e => this.password = e.target.value}
+                        onChange={(e) => (this.password = e.target.value)}
                       />
                       <span className="icon is-small is-left">
                         <i className="fas fa-lock"></i>
@@ -110,7 +109,9 @@ export default class register extends Component {
                         type="password"
                         placeholder="Confirm Password"
                         required
-                        onChange={e => this.password_confirm = e.target.value}
+                        onChange={(e) =>
+                          (this.password_confirm = e.target.value)
+                        }
                       />
                       <span className="icon is-small is-left">
                         <i className="fas fa-lock"></i>
@@ -118,16 +119,15 @@ export default class register extends Component {
                     </div>
                   </div>
                 </div>
-
               </div>
               <div className="field">
                 <div className="control">
                   <button className="button btn is-rounded is-success  ml-6">
                     Sign Up
                   </button>
-                  <label  className="label mt-1 ml-6">
-                  <a href="#">sign-in?</a>
-                </label>
+                  <label className="label mt-1 ml-6">
+                    <a href="#">sign-in?</a>
+                  </label>
                 </div>
               </div>
             </form>
