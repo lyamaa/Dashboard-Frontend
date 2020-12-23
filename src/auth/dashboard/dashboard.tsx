2d08a0dfc6467c674e5a06bfba5dce4dc9b111ec
surajmt8848 <suraj8848mt@gmail.com>
2020-12-23 15:21:41 +0545

    html tags

--- src/auth/dashboard/dashboard.tsx
+++ src/auth/dashboard/dashboard.tsx
@@ -3,7 +3,16 @@ import Wrapper from "../components/wrapper"
 
 const Dashboard = () => (
   <Wrapper>
-    Dashboard
+    <section className="section">
+    <div className="container">
+      <h1 className="title">
+        Hello World
+      </h1>
+      <p className="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
   </Wrapper>
 )
 
