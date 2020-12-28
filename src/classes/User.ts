782bf5a88777839c08fe606ba5922d2c65d7b9f0
surajmt8848 <suraj8848mt@gmail.com>
2020-12-28 23:02:51 +0545

    fix issues

--- src/classes/User.ts
+++ src/classes/User.ts
@@ -1,17 +1,18 @@
 
 import {Role} from "./role"
 export class User {
-    id: number
-    username: string
-    email: string
-    role: Role
-    permissions: string[]
+    id: number;
+    username: string;
+    email: string;
+    role: Role;
+    permissions: string[];
 
-    constructor(id = 0, username="", email="", role= new Role, permissions: any[]){
+    constructor(id = 0, username = '',  email = '', role = new Role(), permissions: string[] = []) {
         this.id = id;
-        this.username = username
-        this.email = email
-        this.role = role
-        this.permissions = permissions
+        this.username = username;
+       
+        this.email = email;
+        this.role = role;
+        this.permissions = permissions;
     }
 }
